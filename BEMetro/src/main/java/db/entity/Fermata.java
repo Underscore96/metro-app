efb23bc15eeb12074df0fcd483b82ec55ccb9986
Vallerga Antonio <96259359+Vallerga@users.noreply.github.com>
2024-04-10 16:13:19 +0200

    entity implemented

---
+++ BEMetro/src/main/java/db/entity/Fermata.java
@@ -0,0 +1,57 @@
+package db.entity;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = { "idFermata" })
+@Entity
+@Table(name = "Expense")
+public class Fermata {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idFermata", nullable = false, unique = true, length = 20)
+	private String idFermata;
+
+	@Column(name = "numFermata", length = 10, nullable = false, unique = true)
+	private Integer numFermata;
+
+	@Column(name = "nome", length = 40, nullable = true, unique = false)
+	private String nome;
+
+	@Column(name = "orarioPrevisto", nullable = true, unique = false)
+	private LocalDate orarioPrevisto;
+
+	@Column(name = "ritardo", nullable = true, unique = false)
+	private LocalDate ritardo;
+
+	@Column(name = "previsioneMeteo", length = 40, nullable = true, unique = false)
+	private String previsioneMeteo;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "idLinea")
+	@JsonIgnore
+	@JsonBackReference
+	private Linea linea;
+}
---
+++ BEMetro/src/main/java/db/entity/Linea.java
@@ -0,0 +1,48 @@
+package db.entity;
+
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = { "idLinea" })
+@Entity
+@Table(name = "Linea")
+public class Linea {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idLinea", nullable = false, unique = true, length = 20)
+	private String idLinea;
+
+	@Column(name = "nomeLinea", length = 10, nullable = false, unique = true)
+	private Integer nomeLinea;
+
+	@Column(name = "direzione", length = 40, nullable = true, unique = false)
+	private String direzione;
+
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "idFermata")
+	@JsonManagedReference
+	@JsonIgnore
+	@ToString.Exclude
+	private Set<Fermata> fermate;
+}

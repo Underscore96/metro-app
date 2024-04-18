d6beea01abe806770581caf2b826e1b012e688a7
Underscore96 <135212755+Underscore96@users.noreply.github.com>
2024-04-18 12:33:33 +0200

    Update MyApplication.java

--- BEMetro/src/main/java/presentation/application/MyApplication.java
+++ BEMetro/src/main/java/presentation/application/MyApplication.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import exception.ServerExceptionMapper;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
+import presentation.provider.ProviderFilter;
 import presentation.rest.FermataFERest;
 import presentation.rest.FermataRest;
 import presentation.rest.GestioneDBRest;
@@ -19,6 +20,7 @@ public class MyApplication extends Application {
 	public Set<Class<?>> getClasses() {
 		Set<Class<?>> s = new HashSet<>();
 		s.add(FermataRest.class);
+		s.add(ProviderFilter.class);
 		s.add(LineaRest.class);
 		s.add(FermataFERest.class);
 		s.add(GestioneDBRest.class);
